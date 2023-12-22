ea57b5acc42a5f93e245be1c4ba78fb9d19ba3a5
Pashs-ba <pashs.ba@gmail.com>
2023-12-22 21:20:48 +0300

    fix small bugs

--- HistFront/src/pages/Game.tsx
+++ HistFront/src/pages/Game.tsx
@@ -3,6 +3,9 @@ import {useEffect, useState} from "react";
 import {GetQuestion} from "../api/quests.ts";
 import {Question} from "../types/Question.ts";
 import {MAX_SCORE, MAX_STEPS} from "../config.ts";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import {Modal as BootstrapModal} from "bootstrap/dist/js/bootstrap.bundle.min.js"
 
 export default function () {
     const {difficultyId} = useParams()
@@ -17,9 +20,13 @@ export default function () {
     const [usedIndexes, setUsedIndexes] = useState([] as number[])
     const [showResult, setShowResult] = useState(false)
     useEffect(() => {
-        window.addEventListener('beforeunload', (event)=>{event.preventDefault()});
+        window.addEventListener('beforeunload', (event) => {
+            event.preventDefault()
+        });
         return () => {
-            window.removeEventListener('beforeunload', (event)=>{event.preventDefault()});
+            window.removeEventListener('beforeunload', (event) => {
+                event.preventDefault()
+            });
         };
     }, []);
     useEffect(() => {
@@ -37,6 +44,29 @@ export default function () {
         [difficultyId]
     )
 
+    function GoNext(){
+        let choose = Math.floor(Math.random() * questions.length)
+        let cc = 0
+        while (usedIndexes.includes(choose)) {
+            choose = Math.floor(Math.random() * questions.length)
+            cc += 1
+            if (cc > 100) {
+                setShowResult(true)
+                break
+            }
+        }
+        if (currentStep + 1 == MAX_STEPS) {
+            setShowResult(true)
+        }
+        setUsedIndexes([...usedIndexes, choose])
+        setCurrentChoice(choose)
+        setAnswers([...Array(questions[choose].answers.length).fill(false)])
+        setColors([...Array(questions[choose].answers.length).fill("black")])
+        setCurrentStep(currentStep + 1)
+        setHintsNumber(0)
+        setMaxScore(MAX_SCORE)
+    }
+
     function CheckAnswers() {
         let newColors = [...colors]
         let fine = 0
@@ -51,26 +81,7 @@ export default function () {
         setMaxScore(maxScore - fine < 1 ? 1 : maxScore - fine)
         if (fine == 0) {
             setScore(score + maxScore)
-            let choose = Math.floor(Math.random() * questions.length)
-            let cc = 0
-            while (usedIndexes.includes(choose)) {
-                choose = Math.floor(Math.random() * questions.length)
-                cc += 1
-                if (cc > 100) {
-                    setShowResult(true)
-                    break
-                }
-            }
-            if (currentStep + 1 == MAX_STEPS) {
-                setShowResult(true)
-            }
-            setUsedIndexes([...usedIndexes, choose])
-            setCurrentChoice(choose)
-            setAnswers([...Array(questions[choose].answers.length).fill(false)])
-            setColors([...Array(questions[choose].answers.length).fill("black")])
-            setCurrentStep(currentStep + 1)
-            setHintsNumber(0)
-            setMaxScore(MAX_SCORE)
+            GoNext()
         } else {
             setColors(newColors)
         }
@@ -79,95 +90,124 @@ export default function () {
 
 
     return (
-        <div className="container my-5">
-            {
-                showResult ? <>
-                    <div className="d-flex justify-content-center flex-column flex-column">
-                        <div className="fs-1 fw-bold">Вы получили {score} баллов</div>
-                        <div className={"text-center"}>
-                            <Link className={'btn btn-primary mt-5 btn-lg'} to={"/"}>На главную</Link>
+        <>
+            <div className="modal fade" id="error"
+                 aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5">Ошибка</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            Нужно выбрать 2 варианта
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
                         </div>
                     </div>
-                </> : (
-                    <>
-                        {
-                            currentChoice == -1 ? <div>Loading...</div> : (
-                                <>
-                                    <div className="fs-3 fw-bold">Шаг {currentStep + 1} из {MAX_STEPS}
-                                        <br/> Баллы: {score}</div>
-                                    <div className="">За решение вы получите {maxScore} баллов</div>
-                                    <div className="d-flex justify-content-center mt-5">
-                                        <img src={questions[currentChoice].imagePath} alt={"some went wrong"}
-                                             className={"mw-100"}/>
-                                    </div>
-                                    {
-                                        hintsNumber > 0 ? (
-                                            <div className="mt-5">
-                                                <div className="fs-4 fw-bold">Подсказки:</div>
-                                                {
-                                                    questions[currentChoice].hints.slice(0, hintsNumber).map((el, index) => {
-                                                        return (
-                                                            <div className="fs-4" key={index}>{el}</div>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        ) : null
-                                    }
-                                    <div className="row mt-5 justify-content-center">
+                </div>
+            </div>
+
+            <div className="container my-5">
+                {
+                    showResult ? <>
+                        <div className="d-flex justify-content-center flex-column flex-column">
+                            <div className="fs-1 fw-bold">Вы получили {score} баллов из {MAX_SCORE * MAX_STEPS}
+                            </div>
+                            <div className={"text-center"}>
+                                <Link className={'btn btn-primary mt-5 btn-lg'} to={"/"}>На главную</Link>
+                            </div>
+                        </div>
+                    </> : (
+                        <>
+                            {
+                                currentChoice == -1 ? <div>Loading...</div> : (
+                                    <>
+                                        <div className="fs-3 fw-bold">Шаг {currentStep + 1} из {MAX_STEPS}
+                                            <br/> Баллы: {score}</div>
+                                        <div className="">За решение вы получите {maxScore} баллов</div>
+                                        <div className="d-flex justify-content-center mt-5">
+                                            <img src={questions[currentChoice].imagePath} alt={"some went wrong"}
+                                                 className={"mw-100"}/>
+                                        </div>
                                         {
-                                            questions[currentChoice].answers.map((el, index) => {
-                                                return (
-                                                    <div className={"col-4 d-flex justify-content-center my-2"}
-                                                         key={index}>
-                                                        <div className="form-check fs-4">
-                                                            <input className={`form-check-input`}
-                                                                   type="checkbox"
-                                                                   checked={answers[index]}
-                                                                   onChange={() => {
-                                                                       if (answers.filter(x => x).length < 2 || answers[index]) {
-                                                                           setAnswers([...answers.slice(0, index), !answers[index], ...answers.slice(index + 1)])
-                                                                       }
-                                                                   }}
-                                                            />
-                                                            <label className={`form-check-label text-${colors[index]}`}
-                                                                   htmlFor="flexCheckDefault">
-                                                                {el.text}
-                                                            </label>
-                                                        </div>
-                                                    </div>
-                                                )
-                                            })
+                                            hintsNumber > 0 ? (
+                                                <div className="mt-5">
+                                                    <div className="fs-4 fw-bold">Подсказки:</div>
+                                                    {
+                                                        questions[currentChoice].hints.slice(0, hintsNumber).map((el, index) => {
+                                                            return (
+                                                                <div className="fs-4" key={index}>{el}</div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            ) : null
                                         }
-                                    </div>
-                                    <div className={"d-flex justify-content-center mt-5"}>
-                                        <button className={"btn btn-primary btn-lg me-3"}
-                                                onClick={() => {
-                                                    if (hintsNumber + 1 <= questions[currentChoice].hints.length) {
-                                                        if (maxScore > 1) {
-                                                            setMaxScore(maxScore - 1)
+                                        <div className="row mt-5 justify-content-center">
+                                            {
+                                                questions[currentChoice].answers.map((el, index) => {
+                                                    return (
+                                                        <div className={"col-4 d-flex justify-content-center my-2"}
+                                                             key={index}>
+                                                            <div className="form-check fs-4">
+                                                                <input className={`form-check-input`}
+                                                                       type="checkbox"
+                                                                       checked={answers[index]}
+                                                                       onChange={() => {
+                                                                           if (answers.filter(x => x).length < 2 || answers[index]) {
+                                                                               setAnswers([...answers.slice(0, index), !answers[index], ...answers.slice(index + 1)])
+                                                                           }
+                                                                       }}
+                                                                />
+                                                                <label
+                                                                    className={`form-check-label text-${colors[index]}`}
+                                                                    htmlFor="flexCheckDefault">
+                                                                    {el.text}
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                        <div className={"d-flex justify-content-center mt-5"}>
+                                            <button className={"btn btn-primary btn-lg me-3"}
+                                                    onClick={() => {
+                                                        if (hintsNumber + 1 <= questions[currentChoice].hints.length) {
+                                                            if (maxScore > 1) {
+                                                                setMaxScore(maxScore - 1)
+                                                            }
+
+                                                            setHintsNumber(hintsNumber + 1)
+                                                        }
+                                                    }}> Подсказка
+                                            </button>
+                                            <button className={"btn btn-primary btn-lg me-3"}
+                                                    onClick={() => {
+                                                        if (answers.filter(x => x).length === 2) {
+                                                            CheckAnswers()
+                                                        } else {
+                                                            const modal = new BootstrapModal(document.getElementById("error"))
+                                                            modal.show()
                                                         }
 
-                                                        setHintsNumber(hintsNumber + 1)
-                                                    }
-                                                }}> Подсказка
-                                        </button>
-                                        <button className={"btn btn-primary btn-lg me-3"}
-                                                onClick={() => {
-                                                    CheckAnswers()
-                                                }}>Проверить ответы
-                                        </button>
-                                        <button className={"btn btn-primary btn-lg me-3"}
-                                                onClick={() => {
-                                                    console.log(answers)
-                                                }}>Пропустить
-                                        </button>
-                                    </div>
-                                </>
-                            )}
-                    </>)
-            }
+                                                    }}>Проверить ответы
+                                            </button>
+                                            <button className={"btn btn-primary btn-lg me-3"}
+                                                    onClick={() => {
+                                                        GoNext()
+                                                    }}>Пропустить
+                                            </button>
+                                        </div>
+                                    </>
+                                )}
+                        </>)
+                }
 
-        </div>
+            </div>
+        </>
     )
 }
